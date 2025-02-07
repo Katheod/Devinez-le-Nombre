31c21043264df8f4268aa9a4114e627e500db44a
Mathis Boivin <mathis.boivin.2@ulaval.ca>
2025-02-07 13:52:37 -0500

    Save

--- Jeu.py
+++ Jeu.py
@@ -1,41 +1,55 @@
 import random
 
-# Version où l'utilisateur doit deviner le nombre que l'ordinateur choisit.
-def devines_le_nombre(x): # Le x est pour que l'utilisateur choisit la difficulté du jeu.
+
+# Fonction pour afficher les messages d'erreur
+def afficher_message_erreur(nombre, nombre_aléatoire):
+    if nombre < nombre_aléatoire:
+        print(f"Désolé, mauvais nombre. Mon nombre est plus grand que {nombre}.")
+    elif nombre > nombre_aléatoire:
+        print(f"Désolé, mauvais nombre. Mon nombre est plus petit que {nombre}.")
+
+# Fonction pour deviner le nombre choisi par l'ordinateur
+def devines_le_nombre(x): 
     nombre_aléatoire = random.randint(1, x)
-    nombre = 0 # Initier la variable.
+    nombre = 0 
     essai = 0
     while nombre_aléatoire != nombre:
-        nombre = int(input(f"Choisis un nombre entre 1 et {x}: "))
-        if nombre < nombre_aléatoire:
-            print(f"Désolé, mauvais nombre. Mon nombre est plus grand que {nombre}")
-            essai += 1
-        elif nombre > nombre_aléatoire: 
-            print(f"Désolé, mauvais nombre. Mon nombre est plus petit que {nombre}")
-            essai += 1
+        try:
+            nombre = int(input(f"Choisis un nombre entre 1 et {x} : "))
+            if 1 <= nombre <= x:
+                afficher_message_erreur(nombre, nombre_aléatoire)
+                essai += 1
+            else:
+                print(f"Veuillez entrer un nombre valide entre 1 et {x}")
+        except ValueError:
+            print(f"Veuillez entrer un nombre valide.")
     
-    print(f"Bravo! Tu as trouvé mon nombre après {essai} essaies. C'était bien le {nombre_aléatoire}")
+    print(f"Bravo! Tu as trouvé mon nombre après {essai} essais. C'était bien le {nombre_aléatoire}")
+
+
+def afficher_resultat(choix, essai):
+    print(f"Ton numéro est le {choix} et je l'ai trouvé après {essai} coups!")
 
 
 def ordinateur_devine(x):
-    min, max = 1, x
-    Retour = ""
+    min_val, max_val = 1, x
+    retour = ""
     essai = 0
-    while Retour != "c":
-        if min != max:
-            choix = random.randint(min, max)
+    while retour != "c":
+        if min_val != max_val:
+            choix = random.randint(min_val, max_val)
         else:
-            choix = min
-        Retour = input(f"Est-ce que {choix} est trop grand (g), trop petit (p) ou correct (c): ").lower()
-        # Trouver filter les nombres restant avec un algorithme de recherche binaire
-        if Retour == "g":
-            max = choix - 1
+            choix = min_val # Ou max. Ça importe peu puisque c'est uniquement si min_val == max_val.
+        retour = input(f"Est-ce que {choix} est trop grand (g), trop petit (p) ou correct (c) ? ").lower()
+        # Filtrer les nombres restant avec un algorithme de recherche binaire.
+        if retour == "g":
+            max_val = choix - 1
             essai += 1
-        elif Retour == "p":
-            min = choix + 1
+        elif retour == "p":
+            min_val = choix + 1
             essai += 1
     
-    print(f"Ton numéro est le {choix} et je l'ai trouvé après {essai} coups")
+    afficher_resultat(choix, essai)
 
 #devines_le_nombre(100)
-ordinateur_devine(100)
+ordinateur_devine(1)
