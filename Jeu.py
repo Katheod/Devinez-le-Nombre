6b8f731e9b8f68be097e9954f98007cff89d129d
Mathis Boivin <mathis.boivin.2@ulaval.ca>
2025-02-06 19:28:49 -0500

    L'ordinateur devine ton nombre

--- Jeu.py
+++ Jeu.py
@@ -1,4 +1,5 @@
 import random
+
 # Version où l'utilisateur doit deviner le nombre que l'ordinateur choisit.
 def devines_le_nombre(x): # Le x est pour que l'utilisateur choisit la difficulté du jeu.
     nombre_aléatoire = random.randint(1, x)
@@ -7,9 +8,26 @@ def devines_le_nombre(x): # Le x est pour que l'utilisateur choisit la difficult
         nombre = int(input(f"Choisis un nombre entre 1 et {x}: "))
         if nombre < nombre_aléatoire:
             print(f"Désolé, mauvais nombre. Mon nombre est plus grand que {nombre}")
-        elif nombre < nombre_aléatoire: 
+        elif nombre > nombre_aléatoire: 
             print(f"Désolé, mauvais nombre. Mon nombre est plus petit que {nombre}")
     
     print(f"Bravo! Tu as trouvé mon nombre. C'était bien le {nombre_aléatoire}")
 
-devines_le_nombre(10)
+
+def ordinateur_devine(x):
+    min, max = 1, x
+    Retour = ""
+    while Retour != "c":
+        if min != max:
+            choix = random.randint(min, max)
+        else:
+            choix = min
+        Retour = input(f"Est-ce que {choix} est trop grand (g), trop petit (p) ou correct (c): ")
+        if Retour == "g":
+            max = choix - 1
+        elif Retour == "g":
+            min = choix + 1
+    
+    print(f"Ton numéro est le {choix}")
+
+ordinateur_devine(100)
